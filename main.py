b13cc9d2f72427223e545a5ab7a215a5eb4d0891
Mof <matheus.o.f.10@gmail.com>
2021-02-21 21:03:15 -0300

    init

---
+++ main.py
@@ -0,0 +1,62 @@
+a = []
+d = []
+
+def h():
+    def j(k):
+            if k == 1:
+                print(len(a))
+            else:
+                print(d.count)
+            
+            print(len(a)+len(d))
+    def i():
+        f = True
+        while f:
+            print("================Adicionar===================")
+            print("1 - Adicionar Filme")
+            print("2 - Adicionar Serie")
+            print("3 - sair")
+            e = int(input("Digite sua opção: "))
+            if e == 1:
+                a.append(input("Digite o nome do Filme: "))
+            elif e == 2:
+                d.append(input("Digite o nome do Serie: "))
+            else:
+                f = False
+            print("Item Adicionado com sucesso")
+
+    def g():
+        print(a + d)
+    def k():
+        l = True
+        while l:
+            print("================Dados dos sistema===================")
+            print("1 - Lista Gereal")
+            print("2 - contagem Series")
+            print("3 - contagem Filmes")
+            print("4 - sair")
+            m = int(input("Digite sua opção: "))
+            if m == 1 :
+                g()
+            elif m == 2 :
+                j(2)
+            elif m == 3 :
+                j(1)
+            else:
+                b = False
+
+    b = True
+    while b:
+        print("================Listas de Filmes e Series===================")
+        print("1 - adicionar nova serie a lista")
+        print("2 - Ver lista geral")
+        print("3 - sair")
+        c = int(input("Digite sua opção: "))
+        if c == 1 :
+            i()
+        elif c == 2 :
+            k()
+        else:
+            b = False
+
+h()

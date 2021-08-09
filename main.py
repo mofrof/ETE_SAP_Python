d0bbf0726dd456c361cacde076d70575333d108e
Mof <matheus.o.f.10@gmail.com>
2021-08-09 14:01:03 -0300

    Termino da pagina de criacao de filmes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Objetos.Filme import Filme
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 
 webApp = Flask(__name__)
 
@@ -13,6 +15,26 @@ filmes = [matrix, vingadores, ghost]
 @webApp.route('/filmes')
 def readFilmes():
     return render_template('lista_filmes.html',listaFimes = filmes)
+
+@webApp.route('/filme/cadastro')
+def cadastroFilme():
+    return render_template('cadastro_filmes.html')
+
+@webApp.route('/filme/cadastrar')
+def createFilme():
+    nome = request.args.get('nome')
+    anoLancamento = request.args.get('anoLancamento')
+    valorDeCompra = request.args.get('valorDeCompra')
+    if(request.args.get('disponivel') == 'on'):
+        disponivel = True
+    else:
+        disponivel = False
+
+    novoFilme = Filme(nome,anoLancamento,disponivel,valorDeCompra)
+
+    filmes.append(novoFilme)
+
+    return redirect('/filmes')
 
 @webApp.route('/')
 def index():

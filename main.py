bbefb1d0bbb3655db9f8d653659f7d382973221d
Mof <matheus.o.f.10@gmail.com>
2021-08-25 09:22:12 -0300

    Termino CRUD

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask.helpers import url_for
 from Objetos.Filme import Filme
 from flask import Flask
 from flask import render_template
@@ -12,13 +13,16 @@ ghost = Filme("Ghost", 1986, False, 355.75)
 
 filmes = [matrix, vingadores, ghost]
 
+
 @webApp.route('/filmes')
 def readFilmes():
-    return render_template('lista_filmes.html',listaFimes = filmes)
+    return render_template('lista_filmes.html', listaFimes=filmes)
+
 
 @webApp.route('/filme/cadastro')
 def cadastroFilme():
     return render_template('cadastro_filmes.html')
+
 
 @webApp.route('/filme/cadastrar')
 def createFilme():
@@ -30,14 +34,47 @@ def createFilme():
     else:
         disponivel = False
 
-    novoFilme = Filme(nome,anoLancamento,disponivel,valorDeCompra)
+    novoFilme = Filme(nome, anoLancamento, disponivel, valorDeCompra)
 
     filmes.append(novoFilme)
 
     return redirect('/filmes')
 
+
+@webApp.route('/filme/excluir')
+def deleteFilme():
+    index = int(request.args.get("index"))
+    filmes.pop(index)
+    return redirect("/filmes")
+
+@webApp.route('/filme/alterar')
+def alterarFilme():
+    index = int(request.args.get("index"))
+    filme = filmes[index]
+    return render_template("alterar_filmes.html", filme = filme, index = index)
+
+@webApp.route('/filme/update')
+def updateFilme():
+    index = int(request.args.get("index"))
+    filme = filmes[index]
+    filme.nome = request.args.get('nome')
+    filme.anoLancamento = request.args.get('anoLancamento')
+    filme.valorDeCompra = request.args.get('valorDeCompra')
+    if(request.args.get('disponivel') == 'on'):
+        filme.disponivel = True
+    else:
+        filme.disponivel = False
+    
+    return redirect('/filmes')
+    
+
+
+
+
+
 @webApp.route('/')
 def index():
     return render_template('index.html')
+
 
 webApp.run(debug="enable")

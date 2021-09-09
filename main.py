1194409eead92fea1d928dc91db1719456426a76
Mof <matheus.o.f.10@gmail.com>
2021-09-08 21:19:24 -0300

    Termino da primeira refatoracao do codigo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask.helpers import url_for
+# from flask.helpers import url_for
 from Objetos.Filme import Filme
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
+from flask import Flask, render_template, request, redirect
 
 webApp = Flask(__name__)
 
@@ -11,7 +8,7 @@ matrix = Filme("Matrix", 1998, True, 150.50)
 vingadores = Filme("Vingadores", 2019, True, 250)
 ghost = Filme("Ghost", 1986, False, 355.75)
 
-filmes = [matrix, vingadores, ghost]
+listaFilmes = [matrix, vingadores, ghost]
 
 def pegarQuantidadeFilmesDisponiveis(listaFilmes):
     quant = 0
@@ -27,54 +24,51 @@ def pegarQuantidadeFilmesIndisponiveis(listaFilmes):
             quant = quant + 1
     return quant
 
-
 @webApp.route('/filmes')
-def readFilmes():
-    return render_template('lista_filmes.html', listaFimes=filmes)
+def mostrarFilmes():
+    return render_template('lista_filmes.html', listaFilmes= listaFilmes)
 
 
 @webApp.route('/filme/cadastro')
-def cadastroFilme():
+def exibirTelaDeCadastroFilme():
     return render_template('cadastro_filmes.html')
 
 
-@webApp.route('/filme/cadastrar')
-def createFilme():
-    nome = request.args.get('nome')
-    anoLancamento = request.args.get('anoLancamento')
-    valorDeCompra = request.args.get('valorDeCompra')
-    if(request.args.get('disponivel') == 'on'):
+@webApp.route('/filme/cadastrar',methods=['POST',])
+def cadastrarFilme():
+    nome = request.form["nome"]
+    anoLancamento = request.form["anoLancamento"] 
+    valorDeCompra = request.form["valorDeCompra"]
+    if "disponivel" in request.form:
         disponivel = True
     else:
         disponivel = False
 
     novoFilme = Filme(nome, anoLancamento, disponivel, valorDeCompra)
 
-    filmes.append(novoFilme)
+    listaFilmes.append(novoFilme)
 
     return redirect('/filmes')
 
 
-@webApp.route('/filme/excluir')
-def deleteFilme():
-    index = int(request.args.get("index"))
-    filmes.pop(index)
+@webApp.route('/filme/excluir/<int:index>')
+def excluirFilme(index):
+    listaFilmes.pop(index)
     return redirect("/filmes")
 
-@webApp.route('/filme/alterar')
-def alterarFilme():
-    index = int(request.args.get("index"))
-    filme = filmes[index]
+@webApp.route('/filme/altera/<int:index>')
+def exibirTelaAlterarFilme(index):
+    filme = listaFilmes[index]
     return render_template("alterar_filmes.html", filme = filme, index = index)
 
-@webApp.route('/filme/update')
-def updateFilme():
-    index = int(request.args.get("index"))
-    filme = filmes[index]
-    filme.nome = request.args.get('nome')
-    filme.anoLancamento = request.args.get('anoLancamento')
-    filme.valorDeCompra = request.args.get('valorDeCompra')
-    if(request.args.get('disponivel') == 'on'):
+@webApp.route('/filme/alterar', methods=['POST',])
+def alterarFilme():
+    index = int(request.form["index"]) 
+    filme = listaFilmes[index]
+    filme.nome = request.form["nome"]
+    filme.anoLancamento = request.form["anoLancamento"]
+    filme.valorDeCompra = request.form["valorDeCompra"]
+    if "disponivel" in request.form:
         filme.disponivel = True
     else:
         filme.disponivel = False
@@ -82,9 +76,9 @@ def updateFilme():
     return redirect('/filmes')
     
 @webApp.route('/')
-def index():
-    quantDisp = pegarQuantidadeFilmesDisponiveis(filmes)
-    quantIndisp = pegarQuantidadeFilmesIndisponiveis(filmes)
+def telaPrincipal():
+    quantDisp = pegarQuantidadeFilmesDisponiveis(listaFilmes)
+    quantIndisp = pegarQuantidadeFilmesIndisponiveis(listaFilmes)
     return render_template('index.html', quantDisp = quantDisp, quantIndisp = quantIndisp)
 
 @webApp.route('/login')
